33b3441993d6161c373a30fa4f57acb1657e3e32
Baton <Baton@Agustins-MacBook-Air.local>
2016-08-14 14:22:55 -0300

    asd

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
 var express = require('express');
 var elasticsearch = require('elasticsearch');
 var app = express();
-var client = new elasticsearch.Client({
-	host:'0.0.0.0:9200',
-	log: 'trace'
-});
+
 
 app.get('/weatherforecast/dia/:diaId', function (req, res) {
-	client.get({
-		index: 'weatherforecast',
-  		type: 'dia',
-  		id: req.params.diaId
-	}, function (error, response) {
-		res.send(response._source);
-	});
+	console.log("entra");
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 8080!');
 });
