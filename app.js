b8b67d84b9dde4f7a9183637932dda8ed5d78ee8
Baton <Baton@Agustins-MacBook-Air.local>
2016-08-14 14:03:19 -0300

    asd

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var elasticsearch = require('elasticsearch');
 var app = express();
 var client = new elasticsearch.Client({
-	host:'localhost:9200',
+	host:'10.128.0.3:9200',
 	log: 'trace'
 });
 
